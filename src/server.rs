80edeba59de85b349bda185469b2d00bd224ebc0
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2025-02-10 21:45:32 +0100

    Add CoAP client

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
 use coap::request::Method;
-use coap::Server;
+use coap::{Server, UdpCoAPClient};
 use coap_lite::CoapRequest;
 use std::net::SocketAddr;
 
@@ -24,11 +24,45 @@ pub(crate) async fn run_coap_server() {
         .unwrap();
 }
 
+#[allow(dead_code)]
+async fn run_coap_client() {
+    let url = "coap://[::1]:5690/1";
+    println!("Client request: {}", url);
+
+    let response = UdpCoAPClient::get(url).await.unwrap();
+    println!(
+        "Server reply: {}",
+        String::from_utf8(response.message.payload).unwrap()
+    );
+}
+
 #[cfg(test)]
 mod tests {
-    use coap::UdpCoAPClient;
+    use super::{run_coap_client, run_coap_server};
+    use coap::request::Method;
+    use coap::{Server, UdpCoAPClient};
+    use coap_lite::CoapRequest;
+    use std::net::SocketAddr;
 
-    use super::run_coap_server;
+    async fn run_test_coap_server() {
+        tokio::spawn(async {
+            let addr = "[::1]:5690";
+            let server = Server::new_udp(addr).unwrap();
+
+            server
+                .run(|mut request: Box<CoapRequest<SocketAddr>>| async {
+                    assert_eq!(request.get_method(), &Method::Get);
+                    assert_eq!(request.get_path(), "1");
+
+                    assert!(&request.response.is_some());
+                    let r = &mut request.response.as_mut().unwrap();
+                    r.message.payload = "OK for GET on /1".as_bytes().to_vec();
+                    request
+                })
+                .await
+                .unwrap();
+        });
+    }
 
     #[tokio::test]
     async fn test_coap_server() {
@@ -42,5 +76,11 @@ mod tests {
             String::from_utf8(response.message.payload).unwrap(),
             "OK for request on request by GET /bs"
         );
+    }
+
+    #[tokio::test]
+    async fn test_coap_client() {
+        run_test_coap_server().await;
+        run_coap_client().await;
     }
 }
